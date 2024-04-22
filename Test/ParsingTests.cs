6143a3c8330a10aaec205affe3e8e003d83bf685
Braedon Wooding <braedonw@resonatesolutions.com.au>
2024-04-22 23:15:02 +1000

    Fix

--- Test/ParsingTests.cs
+++ Test/ParsingTests.cs
@@ -7,7 +7,8 @@ namespace Test
     public class ParsingTests : VerifyBase
     {
         [TestMethod]
-        [DataRow("Declaration", "var x")]
+        [DataRow("Declaration Var", "var x")]
+        [DataRow("Declaration Const", "const y")]
         public async Task TestSimpleExpressions(string name, string data)
         {
             await Verify(Parser.ParseTree(new Lexer().FileToTokens(name, data))).UseMethodName("TestSimpleExpressions_" + name);
